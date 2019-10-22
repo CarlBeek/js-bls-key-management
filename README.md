670a5783d2463fbb5bfb7dd18ba2fe5f989a5dd7
Carl Beekhuizen <carl@ethereum.org>
2019-10-22 13:46:37 +0200

    Implements tree_kdf

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS BLS Key Management
+
+A set of TS packages for the generation and handling of BLS 12-381 keys as per the [BLS 12-381 key standards.](https://github.com/CarlBeek/bls_key_standards)
---
+++ packages/key_derivation/src/tree_kdf.ts
@@ -0,0 +1,41 @@
+import {BigNumber, BitArray} from "sjcl";
+import sjcl from "sjcl";
+
+const r = new sjcl.bn('0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001');
+
+
+function IKM_to_lamport_SK(IKM: BitArray, salt: BitArray): Array<BitArray> {
+    const OKM: BitArray = sjcl.misc.hkdf(IKM, 8160, salt);
+    const bytes_split = (arr: BitArray) => Array.from(new Array(255), (_, i) => arr.slice(i*32, (i+1)*32));
+    return bytes_split(OKM);
+}
+
+
+function parent_SK_to_lamport_PK(parent_SK: BigNumber, index: BigNumber): BitArray {
+    const salt: BitArray = index.toBits(256)
+    const IKM: BitArray = parent_SK.toBits(256)
+    const lamport_0 = IKM_to_lamport_SK(IKM, salt);
+    const flip_bits = (x: BigNumber) => x.sub(new sjcl.bn('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'));
+    const not_IKM: BitArray = flip_bits(parent_SK).toBits(256)
+    const lamport_1 = IKM_to_lamport_SK(not_IKM, salt);
+    const lamport_SK = lamport_0.concat(lamport_1)
+    const lamport_PK = lamport_SK.map((x: BitArray) => sjcl.hash.sha256.hash(x))
+    const combined_lamport_PK = lamport_PK.reduce((a: BitArray, b: BitArray) => a.concat(b))
+    return sjcl.hash.sha256.hash(combined_lamport_PK)
+}
+
+
+function HKDF_mod_r(IKM: BitArray): BigNumber {
+    const OKM: BitArray = sjcl.misc.hkdf(IKM, 48, 'BLS-SIG-KEYGEN-SALT-');
+    const bn_OKM: BigNumber = sjcl.bn.fromBits(OKM);
+    return bn_OKM.mod(r)
+}
+
+
+export function derive_child_SK(parent_SK: BigNumber, index: BigNumber): BigNumber {
+    const compressed_lamport_PK = parent_SK_to_lamport_PK(parent_SK, index)
+    return HKDF_mod_r(compressed_lamport_PK)
+}
+
+
+export const derive_master_SK = HKDF_mod_r
