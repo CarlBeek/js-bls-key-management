9718ddfe47bbc7877954a4d48add96260466ed8f
Carl Beekhuizen <carl@ethereum.org>
2019-10-24 11:31:33 +0200

    Tests as per specs

--- packages/key_derivation/test/tree_kdf_test.ts
+++ packages/key_derivation/test/tree_kdf_test.ts
@@ -1,27 +1,38 @@
 import fs from "fs";
-import { derive_master_SK } from "../src/tree_kdf";
+import { derive_master_SK, derive_child_SK } from "../src/tree_kdf";
 import sjcl from "sjcl";
-import { doesNotReject } from "assert";
 
 
-describe('Derive Master SK', () => {
-    let test_vectors: Array<{[key: string]: string}> = []
-    beforeAll(function(){
-        fs.readFile('packages/key_derivation/test/test_vectors.json', 'utf-8', (err, data) => {
-            if (err) {
-                throw err
-            };
-            test_vectors = JSON.parse(data).kdf_tests;
+test('derive_master_SK()', done => {
+    fs.readFile('packages/key_derivation/test/test_vectors.json', 'utf-8', (err, data) => {
+        if (err) {
+            throw err;
+        };
+        const test_vectors: Array<{[key: string]: string}> = JSON.parse(data).kdf_tests;
+        test_vectors.forEach((test) => {
+            const test_seed = sjcl.codec.hex.toBits(test['seed']);
+            const test_SK = new sjcl.bn(test['master_SK']);
+            const derived_SK = derive_master_SK(test_seed)
+            expect(derived_SK.equals(test_SK)).toBe(true);
         });
     });
-    describe('derive_master_SK()', () => {
-        it('should derive the correct master SK', () => {
-            test_vectors.forEach((test) => {
-                const test_seed = sjcl.codec.hex.toBits(test['seed']);
-                const test_SK = new sjcl.bn(test['master_SK']);
-                console.log(derive_master_SK(test_seed))
-                expect(derive_master_SK(test_seed)).toBe(test_SK)
-            })
-        })
-    })
+    done();
+})
+
+
+test('derive_child_SK()', done => {
+    fs.readFile('packages/key_derivation/test/test_vectors.json', 'utf-8', (err, data) => {
+        if (err) {
+            throw err;
+        };
+        const test_vectors: Array<{[key: string]: string}> = JSON.parse(data).kdf_tests;
+        test_vectors.forEach((test) => {
+            const test_parent_SK = new sjcl.bn(test['master_SK']);
+            const test_index = new sjcl.bn(test['child_index']);
+            const test_child_SK = new sjcl.bn(test['child_SK']);
+            const derived_child_SK = derive_child_SK(test_parent_SK, test_index)
+            expect(derived_child_SK.equals(test_child_SK)).toBe(true);
+        });
+    });
+    done();
 })
